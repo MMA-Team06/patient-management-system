b2744d16b80e5160b8a2a126c020e786cda6603d
Haythem1947 <haythem@example.com>
2025-12-03 22:48:38 +0100

    [SETUP-2] Configure Node.js backend with Express and MySQL dependencies

---
+++ backend/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    '**/*.js',
+    '!**/node_modules/**',
+    '!**/coverage/**',
+    '!jest.config.js'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70
+    }
+  },
+  testMatch: [
+    '**/tests/**/*.test.js'
+  ]
+};
